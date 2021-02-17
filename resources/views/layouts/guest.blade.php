7851cd2a36c203ce13488761df2c1edaa44ef07b
Jens van Wijhe <jens@MBP-van-Jens.fritz.box>
2021-02-17 21:17:53 +0100

    meta desc

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
             <!-- MINIFIED -->
-    {!! SEO::generate(true) !!}
+        {!! SEO::generate(true) !!}
 
 
         <!-- Fonts -->
