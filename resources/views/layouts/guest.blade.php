99943dd37d36aebf035b35e333d6ddebaff42d68
jvwijhe <jensvanwijhe@live.nl>
2021-02-23 12:33:52 +0100

    tagmanager

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,7 +7,13 @@
 
             <!-- MINIFIED -->
             {!! SEO::generate() !!}
-
+        <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5PQZ28T');</script>
+    <!-- End Google Tag Manager -->
 
 
         <!-- Fonts -->
@@ -21,6 +27,11 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body class="bg-black text-white text-lg">
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5PQZ28T"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+            <!-- End Google Tag Manager (noscript) -->
+
         <div class=" antialiased">
             {{ $slot }}
         </div>
